98f43897bc7b946066f0f4508755829a0e3c0a78
Nettle <nettle256@gmail.com>
2017-03-15 14:51:40 +0800

    alpha version finished

--- src/main/resources/static/js/pages/index-ctrl.js
+++ src/main/resources/static/js/pages/index-ctrl.js
@@ -30,4 +30,22 @@ auditApp
             if (id != null && theme != null)
                 $state.go('article', {themeId: theme, id: id});
         }
+    }).filter('cut', function () {
+        return function (value, wordwise, max, tail) {
+            if (!value) return '';
+
+            max = parseInt(max, 10);
+            if (!max) return value;
+            if (value.length <= max) return value;
+
+            value = value.substr(0, max);
+            if (wordwise) {
+                var lastspace = value.lastIndexOf(' ');
+                if (lastspace != -1) {
+                    value = value.substr(0, lastspace);
+                }
+            }
+
+            return value + (tail || ' …');
+        };
     });
